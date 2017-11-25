f63c503937df2fd1a40897e613e5ad5cbe5360cb
Platon Efimov <platon.efimov@gmail.com>
2017-11-26 01:11:00 +0700

    Empty fullscreen game activity

--- app/src/main/java/com/platonefimov/tappydefender/GameActivity.kt
+++ app/src/main/java/com/platonefimov/tappydefender/GameActivity.kt
@@ -1,17 +1,15 @@
 package com.platonefimov.tappydefender
 
-import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
-import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class GameActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_game)
 
         // Fullscreen parameters
         var uiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
@@ -25,14 +23,5 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         // Go Fullscreen
         window.decorView.systemUiVisibility = uiVisibility
         actionBar?.hide()
-
-        // Go to the game activity
-        buttonPlay.setOnClickListener(this)
-    }
-
-    override fun onClick(p0: View?) {
-        val intent = Intent(this, GameActivity::class.java)
-        startActivity(intent)
-        finish()
     }
 }
