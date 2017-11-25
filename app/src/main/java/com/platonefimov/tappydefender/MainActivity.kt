cb0d0075295a585e57f97cc6f7da28e94f50cc0a
Platon Efimov <platon.efimov@gmail.com>
2017-11-25 19:33:09 +0700

    Package rename

---
+++ app/src/main/java/com/platonefimov/tappydefender/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.platonefimov.tappydefender
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.example.platon.tappydefender.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
