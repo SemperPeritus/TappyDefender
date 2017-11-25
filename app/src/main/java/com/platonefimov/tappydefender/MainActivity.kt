7f82f85e217fbe37157fb8cd3d1ace7dbbc8938e
Platon Efimov <platon.efimov@gmail.com>
2017-11-25 20:19:42 +0700

    Go Fullscreen

--- app/src/main/java/com/platonefimov/tappydefender/MainActivity.kt
+++ app/src/main/java/com/platonefimov/tappydefender/MainActivity.kt
@@ -2,12 +2,16 @@ package com.platonefimov.tappydefender
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import com.example.platon.tappydefender.R
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Go Fullscreen
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        actionBar?.hide()
     }
 }
